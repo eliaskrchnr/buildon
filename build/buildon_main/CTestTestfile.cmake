d4207054592b3842b82b633d478abc12c073416e
eliaskirchner@gmx.de <eliaskirchner@gmx.de>
2024-02-08 11:36:53 +0100

    Initial commit

---
+++ build/buildon_main/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/elias/Bachelorarbeit/buildon_ws/src/buildon_main
+# Build directory: /home/elias/Bachelorarbeit/buildon_ws/build/buildon_main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/elias/Bachelorarbeit/buildon_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/elias/Bachelorarbeit/buildon_ws/devel;/home/elias/Bachelorarbeit/ROS_Workspaces/ydlidar_ws/devel;/opt/ros/noetic'.split(';') if '/home/elias/Bachelorarbeit/buildon_ws/devel;/home/elias/Bachelorarbeit/ROS_Workspaces/ydlidar_ws/devel;/opt/ros/noetic' != '' else []
