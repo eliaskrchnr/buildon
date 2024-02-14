c65754a84fdd96ea31b5e8e792eb75ec4d09bb80
eliaskirchner@gmx.de <eliaskirchner@gmx.de>
2024-02-14 22:31:38 +0100

    Remove build devel from Git history

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elias/Bachelorarbeit/buildon_ws/src
-# Build directory: /home/elias/Bachelorarbeit/buildon_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("buildon_main")
-subdirs("ydlidar_ros_driver")
--- build/buildon_main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elias/Bachelorarbeit/buildon_ws/src/buildon_main
-# Build directory: /home/elias/Bachelorarbeit/buildon_ws/build/buildon_main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
